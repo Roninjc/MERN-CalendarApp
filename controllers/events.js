10c5e631052dd0d0211edec087492aeacc1d2494
Jesus <jesus.castanocandela@gmail.com>
2021-10-05 01:34:34 +0200

    Event model and some validation

--- controllers/events.js
+++ controllers/events.js
@@ -10,6 +10,8 @@ const getEvents = ( req, res = response, next ) => {
 };
 
 const createEvent = ( req, res = response, next ) => {
+
+    console.log(req.body);
     
     res.status(200).json({
         ok:true,
--- routes/events.js
+++ routes/events.js
@@ -4,27 +4,41 @@
 */
 const { Router } = require('express');
 const { check } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
 const { validateFields } = require('../middlewares/validate-fields');
 const { validateJWT } = require('../middlewares/validate-jwt');
 
 const router = Router();
 
-router.get( '/', validateJWT, getEvents );
+// Todas las rutas pasan por el middleware validateJWT
+router.use( validateJWT );
 
+
+// Obtener eventos
+router.get( '/', getEvents );
+
+// Crear evento
 router.post(
     '/',
-    validateJWT,
+    [
+        check('title', 'Tittelen er obligatorisk').not().isEmpty(),
+        check('start', 'Startdato er obligatorisk').custom( isDate ),
+        check('start', 'Sluttdato er obligatorisk').custom( isDate ),
+        validateFields
+    ],
     createEvent
 );
 
+// Actualizar evento
 router.put(
     '/:id',
-    validateJWT,
     updateEvent    
 );
 
-router.delete( '/:id', validateJWT, deleteEvent)
+// Borrar evento
+router.delete( '/:id', deleteEvent)
 
 
 module.exports = router;
